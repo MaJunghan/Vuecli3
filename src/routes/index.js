4a936c5e8d39dfbbdf6803e56c7dc4ecf6d9c5c9
MaJunghan <wjdgksak@naver.com>
2021-06-28 23:17:05 +0900

    cli3 router등록

---
+++ src/routes/index.js
@@ -0,0 +1,30 @@
+import { createWebHistory, createRouter } from "vue-router";
+import NewsView from '../components/NewsView'
+import AskView from '../components/AskView.vue';
+import JobsView from '../components/JobsView.vue';
+
+
+const routes = [
+    {
+      path: "/news",
+      name: "NewsView",
+      component: NewsView,
+    },
+    {
+      path: "/ask",
+      name: "AskView",
+      component: AskView,
+    },
+    {
+        path: "/jobs",
+        name: "JobsView",
+        component: JobsView,
+      },
+  ];
+  
+  const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
+  
+  export default router;
