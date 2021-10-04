35606376366cff34a3336b4ae27bb4ff724da36e
MaJunghan <wjdgksak@naver.com>
2021-10-04 23:51:12 +0900

    news

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,10 +1,17 @@
-import { fetchNewsList, fetchAskList, fetchJobsList, fetchUserInfo, fetchItem } from "../api/index";
+import {
+  fetchNewsList,
+  fetchAskList,
+  fetchJobsList,
+  fetchUserInfo,
+  fetchItem,
+} from "../api/index";
 
 export default {
   FETCH_NEWS(context) {
     // 2. actions는 context라는 인자로 mutations에 접근
     fetchNewsList()
       .then((res) => {
+        console.log(res);
         context.commit("SET_NEWS", res.data); // 2. commit이라는 명령어 mutations에 SEW_NEWS를 호출하고 res.data값을 넘겨준다.
       })
       .catch((err) => {
@@ -30,15 +37,13 @@ export default {
       });
   },
   FETCH_USER({ commit }, userName) {
-    fetchUserInfo(userName)
-      .then(({ data }) => {
-        commit("SET_USER", data);
-      })
+    fetchUserInfo(userName).then(({ data }) => {
+      commit("SET_USER", data);
+    });
   },
   FETCH_ITEM({ commit }, itemId) {
-    fetchItem(itemId)
-      .then(({ data }) => {
-        commit("SET_ITEM", data);
-      })
-  }
+    fetchItem(itemId).then(({ data }) => {
+      commit("SET_ITEM", data);
+    });
+  },
 };
